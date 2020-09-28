43cbc8946d43d0e9824f7885699a7399cd264977
Benjamin Gambling <benjamin.gambling@gmail.com>
2020-09-27 21:15:43 -0700

    created link with packages

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
-
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import fetch from "node-fetch";
 import "./styles/index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
 const HOST = window.location.hostname;
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: `https://${HOST}`,
+  fetch: fetch,
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
